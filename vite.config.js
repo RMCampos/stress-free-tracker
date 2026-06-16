360d028b157793ae80e44e3e3f58288199fb2e9a
Ricardo Campos <ricardompcampos@gmail.com>
2026-06-16 20:22:06 -0300

    fix: missing build steps

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/stress-free-tracker/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
