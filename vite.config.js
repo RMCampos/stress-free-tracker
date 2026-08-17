af26c3af07a48ccfab7d0ec2604128a79139328a
Ricardo Campos <ricardompcampos@gmail.com>
2026-08-17 17:07:55 -0300

    Set base path for stress-free-tracker

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/stress-free-tracker/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
